4fdd2f033feb049a5ec79f3994f9c01764158917
shuyuheng <2285128399@qq.com>
2020-01-03 15:42:26 +0800

    sdfasdf

--- db.js
+++ db.js
@@ -1,5 +1,22 @@
 const mysql = require('mysql')
 const config = require('./config.js')
 const connection = mysql.createConnection(config.mysql_config)
-
+// 声明一个同步方法
+connection.queryAsync =  function(sql,data){
+    return new Promise((res) => {
+        connection.query(sql,data,(err,result) => {
+            if(err){
+            console.log(err)
+                res({
+                    ok:0,err
+                })
+            }else{
+                res({
+                    ok:1,
+                    data:result
+                })
+            }
+        })
+    })
+}
 module.exports = connection
